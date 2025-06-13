8177a006287d5e5cbcea17fd61fc780a0b6dd7bd
Prototype0S <edwardcarroll2010@icloud.com>
2025-06-13 14:02:29 +1000

    minor fixes and improvements to ship

--- Objects/Ship.py
+++ Objects/Ship.py
@@ -25,9 +25,11 @@ class Ship(RoomObject):
         """
         
         if key[pygame.K_w]:
-            self.y -= 10
+            #self.y -= 10
+            self.y_speed -= 2
         elif key[pygame.K_s]:
-            self.y += 10
+            #self.y += 10
+            self.y_speed += 2
 
     def keep_in_room(self):
             """
@@ -35,9 +37,11 @@ class Ship(RoomObject):
             """
             if self.y < 0:
                 self.y = 0
+                self.y_speed = 0
             elif self.y + self.height> Globals.SCREEN_HEIGHT:
                 self.y = Globals.SCREEN_HEIGHT - self.height
-                
+                self.y_speed = 0
+
     def step(self):
         """ 
         Determine what happens to the ship on each click of the game clock
